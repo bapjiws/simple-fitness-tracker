b23364d72a438449cde462ee55d46316333e1bba
Alexey Boklin <alex.boklin@gmail.com>
2020-02-18 01:43:59 +0100

    Set up the screen sections

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//<header className="App-header">
+//  <img src={logo} className="App-logo" alt="logo" />
+//  <p>
+//    Edit <code>src/App.js</code> and save to reload.
+//  </p>
+//  <a
+//    className="App-link"
+//    href="https://reactjs.org"
+//    target="_blank"
+//    rel="noopener noreferrer">
+//    Learn React
+//  </a>
+//</header>;
+
+export default () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Progress-Timeline">PT</div>
+      <div className="Measurements">M</div>
     </div>
   );
-}
-
-export default App;
+};
