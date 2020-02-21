859e1105b8ae9e469ee0598d6db7789d266c91a2
Alexey Boklin <alex.boklin@gmail.com>
2020-02-21 16:53:45 +0100

    Clean up unused styles

--- src/App.js
+++ src/App.js
@@ -3,25 +3,10 @@ import ApolloClient from 'apollo-boost';
 import {ApolloProvider} from '@apollo/react-hooks';
 import {useQuery} from '@apollo/react-hooks';
 
-//import logo from './logo.svg';
 import './App.css';
 import {MEASUREMENTS} from './graphql';
 import {ProgressTimeline} from './ProgressTimeline';
 import {Measurements} from './Measurements';
-
-//<header className="App-header">
-//  <img src={logo} className="App-logo" alt="logo" />
-//  <p>
-//    Edit <code>src/App.js</code> and save to reload.
-//  </p>
-//  <a
-//    className="App-link"
-//    href="https://reactjs.org"
-//    target="_blank"
-//    rel="noopener noreferrer">
-//    Learn React
-//  </a>
-//</header>;
 
 const client = new ApolloClient({
   uri: 'http://13.48.85.187:1337/graphql',
